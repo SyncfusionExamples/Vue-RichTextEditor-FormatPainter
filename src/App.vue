27110218ab10f2217d3eb646b4db4bd6b0075fdd
thangavele <thangavele@syncfusion.com>
2024-02-15 19:05:30 +0530

    Addressed the review comments

--- src/App.vue
+++ src/App.vue
@@ -2,25 +2,22 @@
   <div id="defaultRTE">
       <ejs-richtexteditor id="default" :toolbarSettings="toolbarSettings" :formatPainterSettings="formatPainterSettings">
         <h1>The Power of Format Painter in Rich Text Editing</h1>
-        <p>Are you tired of manually reformatting text in your documents? Look no further than the Format Painter, a remarkable feature embedded within Rich Text Editors. This tool empowers users to effortlessly replicate formatting from one text section to another, saving valuable time and effort.</p>
+        <p>Are you tired of manually reformatting text in your documents? Look no further than the Format Painter, a remarkable feature embedded within the Vue Rich Text Editor. This tool empowers users to effortlessly replicate formatting from one text section to another, saving valuable time and effort. </p>
 
         <h2>How to Use the Format Painter</h2>
-        <p>To harness the potential of the Format Painter within a rich text editor, follow these simple steps:</p>
+        <p>To harness the potential of the Format Painter, follow these simple steps:</p>
         <ol>
-        <li>Highlight the text whose formatting you want to replicate.</li>
-        <li>Locate and click on the Format Painter button, often represented by a paintbrush icon, found in the toolbar.</li>
-        <li>Observe as your cursor transforms into a paintbrush icon, indicating the Format Painter's activation.</li>
-        <li>Drag the cursor over the target text where you intend to apply the copied formatting.</li>
-        <li>Release the mouse button to seamlessly transfer the chosen format to the designated text.</li>
+        <li>Highlight the text whose formatting you want to replicate. </li>
+        <li>Locate and click the Format Painter button, often represented by a paintbrush icon, in the toolbar. </li>
+        <li>Observe as your cursor transforms into a paintbrush icon, indicating the Format Painter's activation. </li>
+        <li>Drag the cursor over the target text where you intend to apply the copied formatting. </li>
+        <li>Release the mouse button to seamlessly transfer the chosen format to the designated text. </li>
         </ol>
 
         <h2>Enhance Your Efficiency</h2>
-        <p>The Format Painter is a game-changer when dealing with the challenge of formatting extensive documents. This invaluable tool allows you to swiftly duplicate and apply formatting across multiple sections, eliminating the need for tedious manual adjustments.</p>
-        <p>If you find yourself working with text regularly, whether you're a writer, an editor, or a content creator, the Format Painter will undoubtedly boost your productivity. Say goodbye to repetitive formatting tasks and hello to a more streamlined and efficient editing process.</p>
+        <p>If you find yourself working with text regularly, whether you're a writer, an editor, or a content creator, the Format Painter will undoubtedly boost your productivity. Say goodbye to repetitive formatting tasks and hello to a more streamlined and efficient editing process. </p>
 
-        <p>Experience the convenience of the Format Painter today and revolutionize the way you approach text formatting!</p>
-
-        <p><em>Don't let formatting slow you down. Try the Format Painter and take control of your text editing experience.</em></p>
+        <p>Experience the convenience of the Format Painter today and revolutionize the way you approach text formatting! </p>
       </ejs-richtexteditor>
   </div>
 </template>
